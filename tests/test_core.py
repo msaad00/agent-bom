d910225a2a00e8e360ae41d956b3658a699c4529
Wagdy Saad <andwgdysaad@gmail.com>
2026-02-19 22:58:25 -0500

    fix: update test_version_sync to expect 0.10.0

--- tests/test_core.py
+++ tests/test_core.py
@@ -255,7 +255,7 @@ def empty_report():
 
 def test_version_sync():
     from agent_bom import __version__
-    assert __version__ == "0.9.1"
+    assert __version__ == "0.10.0"
 
 
 def test_report_version_matches():

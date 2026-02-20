5ff43730921304f2a8553dbbe5fba59f767b643c
Wagdy Saad <andwgdysaad@gmail.com>
2026-02-20 03:03:50 -0500

    chore: bump version to 0.12.2 for PyPI README update

--- src/agent_bom/__init__.py
+++ src/agent_bom/__init__.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("agent-bom")
 except PackageNotFoundError:
-    __version__ = "0.12.1"
+    __version__ = "0.12.2"

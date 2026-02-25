d13999589ac6f15cf23e2d86b23a3800f062341f
W S <34316639+msaad00@users.noreply.github.com>
2026-02-24 22:17:51 -0500

    chore: bump version to v0.32.0 (#38)

--- src/agent_bom/__init__.py
+++ src/agent_bom/__init__.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("agent-bom")
 except PackageNotFoundError:
-    __version__ = "0.31.9"
+    __version__ = "0.32.0"
